21e3576dcb68c3f474b704c376d97eaadce86c6c
15ec003 <akilasakthivel97@gmail.com>
2018-07-24 12:11:49 +0530

    Create positive or negative or zero.C

---
+++ BEGINNER LEVEL/positive or negative or zero.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if (a==0)
+{
+printf("ZERO");
+}
+else if(a>0)
+{
+printf("POSITIVE");
+}
+else
+{
+printf("NEGATIVE");
+}
+return 0;
+}
